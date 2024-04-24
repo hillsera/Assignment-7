1c66b39fa763663c0d76401ceaa00f13d9b088da
Sera Hill <sahill1@buffs.wtamu.edu>
2024-04-23 21:30:02 -0500

    added tests for assignment 7

--- Barky2024_Refactor_3/src/djbarky/barkyarch/tests.py
+++ Barky2024_Refactor_3/src/djbarky/barkyarch/tests.py
@@ -44,3 +44,54 @@ class TestCommands(TestCase):
 
         # that object is the same as the one we inserted
         self.assertEqual(Bookmark.objects.get(id=1).url, self.domain_bookmark_1.url)
+
+    # Added this to test listing bookmarks
+    def test_command_list_default_order(self):
+        add_command = AddBookmarkCommand()
+        add_command.execute(self.domain_bookmark_1)
+        add_command.execute(self.domain_bookmark_2)
+
+        list_command = ListBookmarksCommand()
+        result = list_command.execute()
+
+        self.assertEqual(len(result), 2)
+
+        self.assertEqual(result[0].id, self.domain_bookmark_1.id)
+        self.assertEqual(result[1].id, self.domain_bookmark_2.id)
+
+    # Added this to test deleting bookmarks
+    def test_command_delete(self):
+        add_command = AddBookmarkCommand()
+        add_command.execute(self.domain_bookmark_1)
+
+        self.assertEqual(Bookmark.objects.count(), 1)
+
+        delete_command = DeleteBookmarkCommand()
+        delete_command.execute(self.domain_bookmark_1)
+
+        self.assertEqual(Bookmark.objects.count(), 0)
+
+    # Added this to test editing bookmarks
+    def test_command_edit(self):
+        # Modify the data for the first bookmark
+        modified_data = DomainBookmark(
+            id=1,
+            title="Awesomer Django",
+            url="https://awesomedjango.org/",
+            notes="Best place on the web for Django just got better.",
+            date_added=self.domain_bookmark_1.date_added,
+        )
+
+        # Create an instance of EditBookmarkCommand
+        edit_command = EditBookmarkCommand()
+
+        # Execute the edit command
+        edit_command.execute(modified_data)
+
+        # Fetch the updated bookmark from the database
+        updated_bookmark = Bookmark.objects.get(id=1)
+
+        # Check if the bookmark attributes have been updated correctly
+        self.assertEqual(updated_bookmark.title, modified_data.title)
+        self.assertEqual(updated_bookmark.url, modified_data.url)
+        self.assertEqual(updated_bookmark.notes, modified_data.notes)
